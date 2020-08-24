722eec91748e8da3d9e514a47c9556a31052ee49
depstor <depstor90@gmail.com>
2020-08-25 01:04:33 +0300

    timer

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,7 +1,7 @@
 "use strikt"
 
 window.addEventListener('DOMContentLoaded', () => {
-
+    //Tabs
     const tabs = document.querySelectorAll('.tabheader__item'),
         tabsContent = document.querySelectorAll('.tabcontent'),
         tabsParent = document.querySelector('.tabheader__items');
@@ -38,4 +38,73 @@ window.addEventListener('DOMContentLoaded', () => {
                 }
             });
     });
+
+    //Timer
+
+    const deadline = '2020-08-26';
+
+    function getTimeRemaming(endtime) {
+        const t = Date.parse(endtime) - Date.parse(new Date()),
+            days = Math.floor(t / (1000 * 60 * 60 * 24)),
+            hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            seconds = Math.floor((t / 1000) % 60);
+
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds,
+        };
+    }
+
+    function getZero(num) {
+        if (num >= 0 && num < 10) {
+            return `0${num}`;
+        } else {
+            return num
+        }
+    }
+
+    function setClock(selector, endtime) {
+        const timer = document.querySelector(selector),
+            days = timer.querySelector('#days'),
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            seconds = timer.querySelector('#seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        updateClock();
+
+        function updateClock() {
+            const t = getTimeRemaming(endtime);
+
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+
+            function endTimer(value, selector) {
+                if (value <= 0) {
+                    selector.innerHTML = "00";
+                } else {
+                    selector.innerHTML = getZero(value);
+                }
+            }
+
+            endTimer(t.days, days);
+            endTimer(t.hours, hours);
+            endTimer(t.minutes, minutes);
+            endTimer(t.seconds, seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    setClock('.timer', deadline);
+
+    console.log(new Date())
 });
