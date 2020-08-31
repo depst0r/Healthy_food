b2c0125cade6e90d74a841e5b776b268f43bc0d1
depstor <depstor90@gmail.com>
2020-09-01 00:13:55 +0300

    01.09.2020

--- dist/js/script.js
+++ dist/js/script.js
@@ -255,10 +255,6 @@ window.addEventListener('DOMContentLoaded', () => {
             `;
             form.insertAdjacentElement('afterend', statusMessage);
 
-            const request = new XMLHttpRequest();
-            request.open("POST", "server.php");
-
-            request.setRequestHeader('Content-type', 'application/json');
             const formData = new FormData(form);
 
             const object = {};
@@ -266,20 +262,20 @@ window.addEventListener('DOMContentLoaded', () => {
                 object[key] = value;
             });
 
-            const json = JSON.stringify(object);
-
-            request.send(json);
-
-            request.addEventListener('load', () => {
-                if (request.status === 200) {
-                    console.log(request.response);
+            fetch('server.php', {
+                    method: 'POST',
+                    headers: {
+                        'Content-type': 'application/json'
+                    },
+                    body: JSON.stringify(object)
+                }).then(data => data.text())
+                .then(data => {
+                    console.log(data);
                     showThanksModal(message.success);
-                    form.reset();
                     statusMessage.remove();
-                } else {
-                    showThanksModal(message.failure);
-                }
-            });
+                })
+                .catch(() => showThanksModal(message.failure))
+                .finally(() => form.reset());
         });
     };
 
@@ -306,4 +302,16 @@ window.addEventListener('DOMContentLoaded', () => {
             closeModal();
         }, 4000);
     }
+
+    fetch('https://jsonplaceholder.typicode.com/posts/', {
+            method: "POST",
+            body: JSON.stringify({
+                name: 'Alex'
+            }),
+            headers: {
+                'Content-type': 'application/json'
+            }
+        })
+        .then(response => response.json())
+        .then(json => console.log(json));
 });
