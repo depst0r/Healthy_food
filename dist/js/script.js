dfc30d9b55f5761ec9ec43741adb5ce3c8242ebf
depstor <depstor90@gmail.com>
2020-09-10 10:50:02 +0300

    axios

--- dist/js/script.js
+++ dist/js/script.js
@@ -208,9 +208,16 @@ window.addEventListener('DOMContentLoaded', () => {
         return await res.json();
     };
 
-    getResource('http://localhost:3000/menu')
-        .then(data => {
-            data.forEach(({ img, altimg, title, descr, price }) => {
+    // getResource('http://localhost:3000/menu')
+    //     .then(data => {
+    //         data.forEach(({ img, altimg, title, descr, price }) => {
+    //             new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+    //         });
+    //     });
+
+    axios.get('http://localhost:3000/menu')
+        .then(res => {
+            res.data.forEach(({ img, altimg, title, descr, price }) => {
                 new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
             });
         });
@@ -291,9 +298,4 @@ window.addEventListener('DOMContentLoaded', () => {
             closeModal();
         }, 4000);
     }
-
-    // fetch('http://localhost:3000/menu ')
-    //     .then(data => data.json())
-    //     .then(res => console.log(res))
-
 });
